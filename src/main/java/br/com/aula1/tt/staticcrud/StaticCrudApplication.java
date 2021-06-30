09184adafa267b8c1e5247dd60b4013747c0bc39
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-30 15:35:47 -0300

    Inicializando projeto spring boot

---
+++ src/main/java/br/com/aula1/tt/staticcrud/StaticCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.aula1.tt.staticcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/aula1/tt/staticcrud/StaticCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula1.tt.staticcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

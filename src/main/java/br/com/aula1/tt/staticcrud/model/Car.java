14f5804ecd3624a2ce78ec2c043c3d5a659ef5c6
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-30 15:38:40 -0300

    Criação da classe Model do Carro

---
+++ src/main/java/br/com/aula1/tt/staticcrud/model/Car.java
@@ -0,0 +1,38 @@
+package br.com.aula1.tt.staticcrud.model;
+
+public class Car {
+
+    private String modelo;
+    private String marca;
+    private Integer ano;
+
+    public Car(String modelo, String marca, Integer ano) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+}
